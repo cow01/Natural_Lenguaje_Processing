8fe68e5cb815a99d8ec51ba9bbca6dc84d9bfe78
cow01 <95662557+cow01@users.noreply.github.com>
2022-05-06 19:05:11 -0500

    Add files via upload

---
+++ nlp/Pra2.py
@@ -0,0 +1,19 @@
+'''
+Axel Hervey Cruz Baez
+'''
+carpeta_nombre="C:\\Users\\axelb\\Desktop\\nlp\\"
+
+archivo_nombre="doc3.txt"
+
+palabra=input("Ingresa la palabra a buscar en el documento: ")
+
+with open(carpeta_nombre+archivo_nombre,"r") as archivo:
+    texto=archivo.read()
+
+if palabra in texto:
+    print("La palabra",palabra,"fue encontrada en el texto")
+
+else:
+    print("La palabra",palabra," no fue encontrada en el texto")
+
+        
